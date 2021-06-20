3033fa4b25c5a62eaf4d8aab44f7abc2df4381c9
Yehuda1977 <42040644+Yehuda1977@users.noreply.github.com>
2021-06-20 14:59:32 +0300

    third commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env/ node
+#!/usr/bin/env node
 
 
 // https://medium.com/northcoders/creating-a-project-generator-with-node-29e13b3cd309
@@ -47,26 +47,24 @@ inquirer.prompt(QUESTIONS)
 // Step 3: Recursively call createDirectoryContents for any nested directories
 
 function createDirectoryContents (templatePath, newProjectPath) {
-  const filesToCreate = fs.readdirSync(templatePath);
-
-  filesToCreate.forEach(file => {
-    const origFilePath = `${templatePath}/${file}`;
-    
-    // get stats about the current file
-    const stats = fs.statSync(origFilePath);
-
-    if (stats.isFile()) {
-        const contents = fs.readFileSync(origFilePath, 'utf8');
+    const filesToCreate = fs.readdirSync(templatePath);
   
-        // Rename
-        if (file === '.npmignore') file = '.gitignore';
+    filesToCreate.forEach(file => {
+      const origFilePath = `${templatePath}/${file}`;
       
+      // get stats about the current file
+      const stats = fs.statSync(origFilePath);
+  
+      if (stats.isFile()) {
+        const contents = fs.readFileSync(origFilePath, 'utf8');
+        
         const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
         fs.writeFileSync(writePath, contents, 'utf8');
-    }
-  });
-}
-
-
-
- 
+      } else if (stats.isDirectory()) {
+        fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
+        
+        // recursive call
+        createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
+      }
+    });
+  }
