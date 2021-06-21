c6143cea7dcf6ce3b2163d6f4d22418c927efb3f
Yehuda1977 <42040644+Yehuda1977@users.noreply.github.com>
2021-06-21 09:40:13 +0300

    fifth commit

---
+++ bin/create-twik-project.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+const fs = require('fs-extra');
+const inquirer = require('inquirer');
+
+
+
+const QUESTIONS = require('../lib/questions');
+
+const CURR_DIR = process.cwd();
+
+inquirer.prompt(QUESTIONS)
+  .then(answers => {
+    const projectChoice = answers['project-choice'];
+    const projectName = answers['project-name'];
+    const templatePath = `../templates/${projectChoice}`;
+  
+    fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+
+    createDirectoryContents(templatePath, projectName);
+  });
+
+// Step 3: Recursively call createDirectoryContents for any nested directories
+
+function createDirectoryContents (templatePath, newProjectPath) {
+    const filesToCreate = fs.readdirSync(templatePath);
+  
+    filesToCreate.forEach(file => {
+      const origFilePath = `${templatePath}/${file}`;
+      
+      // get stats about the current file
+      const stats = fs.statSync(origFilePath);
+  
+      if (stats.isFile()) {
+        const contents = fs.readFileSync(origFilePath, 'utf8');
+        
+        const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
+        fs.writeFileSync(writePath, contents, 'utf8');
+      } else if (stats.isDirectory()) {
+        fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
+        
+        // recursive call
+        createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
+      }
+    });
+  }
